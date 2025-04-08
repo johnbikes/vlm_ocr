156db96888f1b2e7e9d5fb4e276f5ee448e9b51f
johnbikes <474683+johnbikes@users.noreply.github.com>
2025-04-08 17:50:17 -0400

    simplifying convert to use utils

--- convert.py
+++ convert.py
@@ -4,27 +4,19 @@ from pathlib import Path
 from dotenv import load_dotenv
 from pdf2image import convert_from_path
 
+from utils import get_pdf_images
+
 
 def main():
     load_dotenv()
 
-    pdf_dir = os.getenv("PDF_DIR")
-    
-    print(f"{pdf_dir = }")
+    # will use the env if pdf_dir_path not passed in
+    pdf_images_dict = get_pdf_images(filter=os.getenv("PDF_DIR_FILTER"))
 
-    assert pdf_dir is not None, f"{pdf_dir} must be set"
-    pdf_dir_path: Path = Path(pdf_dir)
-    assert pdf_dir_path.is_dir(), f"{pdf_dir_path} must be a directory"
+    for path_name, pdf_images in pdf_images_dict.items():
 
-    for path_i, pdf_path in enumerate(pdf_dir_path.glob('*.pdf')):
-        print(f"Converting {pdf_path} to image")
-    
-        images = convert_from_path(str(pdf_path))
-
-        print('images', images)
-
-        for im_i, image in enumerate(images):
-            image.save(f"{path_i}_{im_i}.png")
+        for im_i, image in enumerate(pdf_images):
+            image.save(f"{path_name}_{im_i}.png")
 
 if __name__ == '__main__':
     main()
--- utils.py
+++ utils.py
@@ -1,8 +1,10 @@
 # code ref: https://github.com/johnbikes/vits-cifar10/blob/main/test_env.py
 
 import os
+from pathlib import Path
 
 from dotenv import load_dotenv
+from pdf2image import convert_from_path
 
 
 def configur_env():
@@ -25,3 +27,34 @@ def configur_env():
         if hf_ds_cache is not None:
             os.environ['HF_DATASETS_CACHE'] = hf_ds_cache
             print('Using HF_DATASETS_CACHE')
+
+def get_pdf_images(pdf_dir_path: Path = None, filter: str = None):
+    print(f"grabbing pdf images using {filter = }")
+
+    # grab from the env if not set
+    if pdf_dir_path is None:
+        pdf_dir = os.getenv("PDF_DIR")
+        print(f"from env: {pdf_dir = }")
+        assert pdf_dir is not None, f"{pdf_dir} must be set"
+        pdf_dir_path = Path(pdf_dir)
+
+    assert pdf_dir_path.is_dir(), f"{pdf_dir_path} must be a directory"
+
+    images_dict = {}
+    for path_i, pdf_path in enumerate(pdf_dir_path.glob('*.pdf')):
+        if filter is not None and filter.lower() not in pdf_path.stem.lower():
+            print(f"Skipping {pdf_path} because it does not match filter")
+            continue
+
+        print(f"Converting {pdf_path} to image")
+    
+        images = convert_from_path(str(pdf_path))
+        
+        # print('images', images)
+        
+        # for im_i, image in enumerate(images):
+        #     image.save(f"{path_i}_{im_i}.png")
+
+        images_dict[pdf_path.stem] = images
+
+    return images_dict
